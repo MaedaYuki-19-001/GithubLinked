5a0f87af623dbad8d85a1d607916363b35a941ad
Maeda-Yuki <1910370001x@kindai.ac.jp>
2021-07-16 19:23:21 +0900

    Initial Commit

---
+++ GithubLinked/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubLinked
+//
+//  Created by 前田　祐希 on 2021/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubLinked/GithubLinkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubLinkedApp.swift
+//  GithubLinked
+//
+//  Created by 前田　祐希 on 2021/07/16.
+//
+
+import SwiftUI
+
+@main
+struct GithubLinkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
